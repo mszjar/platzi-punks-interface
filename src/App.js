b48c84518eac00041f2f63555ad8524468997e2b
mszjar <mario.sanchez.jar@gmail.com>
2022-09-03 17:17:01 +0200

    layout initialized

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Web3 from "web3/dist/web3.min";
+/* import Web3 from "web3/dist/web3.min"; */
 import Home from "./views/home";
+import MainLayout from './layouts/main';
 
 function App() {
-  useEffect(() => {
-    if(window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
-    }, []);
   return (
-    <>
-    <Routes>
-      <Route path="/" exact element={<Home/>} />
-    </Routes>
-    </>
+    <MainLayout>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+      </Routes>
+    </MainLayout>
   );
 }
 
