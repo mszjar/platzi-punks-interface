bd18e6145eb3805e6821629b2934c1a82bfcf85c
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-09-03 14:40:58 +0200

    react-app and web3 set up - webpack5 issue fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Web3 from "web3";
+import Web3 from "web3/dist/web3.min";
 import Home from "./views/home";
 
 function App() {
   useEffect(() => {
     if(window.ethereum) {
-      window.ethereum
-        .request({method: 'eth_requestAccounts'})
-        .then((accounts) => console.log(accounts))}
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
     }
-  );
+    }, []);
   return (
     <>
     <Routes>
@@ -21,7 +20,3 @@ function App() {
 }
 
 export default App;
-
-
-/*     const web3 = new Web3(window.ethereum);
-    web3.eth.requestAccounts().then(console.log); */
