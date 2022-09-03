e08635c8a5808d5c038a835c6f87b7c13d06ad38
mszjar <mario.sanchez.jar@gmail.com>
2022-09-04 00:33:01 +0200

    abi from platziPunks smart contract imported

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,28 @@
+import { useCallback, useEffect, useState } from 'react';
+import usePlatziPunks from '../../hooks/usePlatziPunks';
+import { useWeb3React } from '@web3-react/core';
+
 const Home = () => {
+    const { active } = useWeb3React();
+    const [maxSupply, setMaxSupply] = useState();
+
+    const platziPunks = usePlatziPunks();
+
+    const getMaxSupply = useCallback( async () => {
+        if(platziPunks) {
+            const result = await platziPunks.methods.maxSupply().call();
+            setMaxSupply(result);
+        }
+    }, [platziPunks]);
+
+    useEffect(()=> {
+        getMaxSupply();
+    }, [getMaxSupply]);
+
+    if(!active) return "Conecta tu wallet";
     return (
         <>
-            <p>Hello World</p>
+            <p>Max supply: {maxSupply}</p>
         </>
     )
 }
